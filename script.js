412042c4f902244967583b2f6842391a587ab88f
Rohan C. Shah <rcs353@cornell.edu>
2023-10-28 14:19:35 -0400

    new changes

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  showHome();
+});
+
+
+function showContent(contentId) {
+  const contents = document.querySelectorAll('.content');
+  contents.forEach(content => {
+    content.style.display = 'none';
+  });
+
+  const targetContent = document.getElementById(contentId);
+  if (targetContent) {
+    targetContent.style.display = 'block';
+  }
+}
+
+function showHome() {
+  showContent('home');
+}
+
+function showAboutMe() {
+  showContent('more-abt-me');
+}
+
+function showExperiences() {
+  showContent('stuff-ive-done');
+}
+
+function showContact() {
+  showContent('connect');
+}
